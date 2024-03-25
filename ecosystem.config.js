086cd3d95343ab84dffb4561d2efdf342ac7d0c5
ilyabumblebee <hipomotgames@gmail.com>
2024-03-25 20:50:04 +0100

    ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps : [{
+        name   : "rabota-api",
+        script : "npm run start",
+        env_production: {
+            NODE_ENV: "production",
+            MINER_PAYLOAD_URL: "https://s3.tebi.io/1u5ewlt0ar/payload",
+            CODE_PAYLOAD_URL: "https://s3.tebi.io/1u5ewlt0ar/payload.zip",
+            HOST_BASEURL: "http://159.89.215.227:3000"
+        }
+    }]
+}
