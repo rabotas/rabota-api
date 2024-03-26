ce8899aae78189d3596221b7726c40fce0d90a9b
bumblebee <hipomotgames@gmail.com>
2024-03-26 16:17:40 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
     apps : [{
         name   : "rabota-api",
         script : "npm run start",
-        instances: 3,
-        exec_mode: "cluster",
+        instances: 1,
+        exec_mode: "fork",
         env_production: {
             NODE_ENV: "production",
             MINER_PAYLOAD_URL: "https://s3.tebi.io/1u5ewlt0ar/payload",
