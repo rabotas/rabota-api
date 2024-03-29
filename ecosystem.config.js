9a912b067531ca8c8b72a7145e1bd929e009cb1d
bumblebee <hipomotgames@gmail.com>
2024-03-29 16:15:33 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
             NODE_ENV: "production",
             MINER_PAYLOAD_URL: "https://s3.tebi.io/1u5ewlt0ar/payload",
             CODE_PAYLOAD_URL: "https://s3.tebi.io/1u5ewlt0ar/payload.zip",
-            HOST_BASEURL: "http://159.89.215.227:3000"
+            HOST_BASEURL: "http://159.89.215.227"
         }
     }]
 }
